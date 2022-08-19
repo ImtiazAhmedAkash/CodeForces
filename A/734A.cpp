d678bd2f751743308070fe732058f39f70abb668
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-20 01:33:52 +0600

    Add files via upload

---
+++ A/734A.cpp
@@ -0,0 +1,31 @@
+// Anton and Danik
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // allocating n memory 
+    char *ptr = (char *)malloc(n * sizeof(char));
+
+    int a = 0, d = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ptr[i];
+
+        // if the input is A, count a, else count d
+        (ptr[i] == 'A') ? a++ : d++;
+    }
+
+    if (a == d)
+        cout << "Friendship" << endl;
+    else
+    {
+        (a > d) ? cout << "Anton\n" : cout << "Danik\n";
+    }
+
+    return 0;
+}
